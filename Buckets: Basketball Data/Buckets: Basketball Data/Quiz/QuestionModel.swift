f70f3543bf15600c2fd78d9e7faa746f705b5939
rrbolton423 <bolton.romell423@gmail.com>
2019-10-04 11:27:44 -0400

    Update quiz questions

--- Buckets: Basketball Data/Buckets: Basketball Data/Quiz/QuestionModel.swift
+++ Buckets: Basketball Data/Buckets: Basketball Data/Quiz/QuestionModel.swift
@@ -10,16 +10,16 @@ import GameKit
 
 struct QuestionModel {
     let questions = [
-        Question(interrogative: "Who invented the game of basketball?", answers: ["Frank Mahan", "Abner Doubleday", "Walter Camp", "James Naismith"], correctAnswerIndex: 3),
-        Question(interrogative: "What teammates were nicknamed the \"Splash Brothers\"?", answers: ["Jerry West & Wilt Chamberlain", "Kevin Durant & Russell Westbrook", "Michael Jordan & Scottie Pippen", "Stephen Curry & Klay Thompson"], correctAnswerIndex: 3),
-        Question(interrogative: "What player holds the NBA record for the most career assists?", answers: ["Jason Kidd", "John Stockton", "Magic Johnson", "Steve Nash"], correctAnswerIndex: 1),
-        Question(interrogative: "Who was the first NBA player to record a triple-double in the All-Star Game?", answers: ["Dwyane Wade", "John Stockton", "LeBron James", "Michael Jordan"], correctAnswerIndex: 3),
-        Question(interrogative: "Who is the highest-scoring foreign-born player in NBA history?", answers: ["Dirk Nowitzki", "Yao Ming", "Hakeem Olajuwon", "Detlef Schrempf"], correctAnswerIndex: 0),
-        Question(interrogative: "Who is the NBA Championship trophey named after?", answers: ["Larry O'Brien", "Maurice Podoloff", "Red Auerbach", "James Naismith"], correctAnswerIndex: 0),
-        Question(interrogative: "Which NBA team plays at Madison Square Garden?", answers: ["Brooklyn Nets", "Miami Heat", "New York Knicks", "Golden State Warriors"], correctAnswerIndex: 2),
-        Question(interrogative: "What NBA player has won the most league MVPs?", answers: ["Michael Jordan", "LeBron James", "Kareem Abdul-Jabbar", "Stephen Curry"], correctAnswerIndex: 2),
-        Question(interrogative: "Which team owns the longest winning streak in NBA history?", answers: ["Chicago Bulls", "Los Angeles Lakers", "Miami Heat", "Golden State Warriors"], correctAnswerIndex: 1),
-        Question(interrogative: "Who has the most blocked shots in NBA history?", answers: ["Mark Eaton", "Dikembe Mutombo", "Hakeem Olajuwon", "Kareem Abdul-Jabbar"], correctAnswerIndex: 2)
+        Question(interrogative: "Which of the following is NOT a nickname of Vince Carter?", answers: ["Jurassic Park", "Vinsanity", "Half-Man, Half-Amazing", "No one knows", "Air Canada"], correctAnswerIndex: 0),
+        Question(interrogative: "What is depicted on the logo of the Golden State Warriors?", answers: ["A city skyline", "A bridge", "A spearhead", "A basketball"], correctAnswerIndex: 1),
+        Question(interrogative: "Who was the shortest player to win an MVP?", answers: ["Steve Nash", "Charles Barkley", "Spud Webb", "Allen Iverson"], correctAnswerIndex: 3),
+        Question(interrogative: "What was the \"process\" of 76ers general manager Sam Hinkie?", answers: ["Looking for players in Europe", "Tanking for several years in a row", "Only drafting excellent 3 point shooters", "Signing veteran leaders"], correctAnswerIndex: 1),
+        Question(interrogative: "About what percent of NBA players are black, as of 2015?", answers: ["50", "95", "25", "75"], correctAnswerIndex: 3),
+        Question(interrogative: "What country is Manu Ginóbili from?", answers: ["France", "United States", "Argentina", "Italy"], correctAnswerIndex: 2),
+        Question(interrogative: "What is the record for most regular season wins, set by the Golden State Warriors in 2015-16?", answers: ["69", "50", "73", "82"], correctAnswerIndex: 2),
+        Question(interrogative: "What team used to be the Seattle Supersonics?", answers: ["Oklahoma City Thunder", "Phoenix Suns", "San Antonio Spurs", "Orlando Magic"], correctAnswerIndex: 0),
+        Question(interrogative: "When Kevin Durant won the MVP award in 2014, he said someone else was \"da real MVP\". Who was it?", answers: ["Barack Obama", "Lebron James", "Russell Westbrook", "His mom"], correctAnswerIndex: 3),
+        Question(interrogative: "Which of the following was never a teammate of Michael Jordan?", answers: ["Toni Kukoč", "Dennis Rodman", "Grant Hill", "Steve Kerr"], correctAnswerIndex: 2)
     ]
     
     var previouslyUsedNumbers: [Int] = []
