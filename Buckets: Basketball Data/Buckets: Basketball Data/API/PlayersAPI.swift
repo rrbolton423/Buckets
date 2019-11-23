b29d8f631ad943765c6ff7b23d9537c2c557e454
rrbolton423 <bolton.romell423@gmail.com>
2019-11-23 00:17:13 -0500

    Update 1.15

--- Buckets: Basketball Data/Buckets: Basketball Data/API/PlayersAPI.swift
+++ Buckets: Basketball Data/Buckets: Basketball Data/API/PlayersAPI.swift
@@ -12,7 +12,6 @@ import SwiftyJSON
 class PlayersApi {
     func getPlayers(url: String, completion: @escaping ([Players]?, Error?) -> Void) {
         var resultArray = [Players]()
-        guard let url = URL(string: url) else { return }
         var ID: String?
         var fullName: String?
         var firstName: String?
@@ -22,55 +21,63 @@ class PlayersApi {
         var position: String?
         var teamID: String?
         var fullNameArr: [String]?
-        do {
-            let data = try Data(contentsOf: url)
-            let json = try JSON(data: data)
-            _ = json["resultSets"][0]["headers"]
-            let rowSet = json["resultSets"][0]["rowSet"]
-            if rowSet.count == 0 {
-                if let jsonArray = rowSet.array
-                {
-                    for _ in jsonArray
+        guard let headerUrl = URL(string: url) else { return }
+        var request = URLRequest(url: headerUrl)
+        request.httpMethod = "GET"
+        request.setValue("stats.nba.com", forHTTPHeaderField:"Referer")
+        request.timeoutInterval = 60.0
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else { print(error!); return }
+            guard let data = data else { print("No data"); return }
+            do {
+                let json = try JSON(data: data)
+                _ = json["resultSets"][0]["headers"]
+                let rowSet = json["resultSets"][0]["rowSet"]
+                if rowSet.count == 0 {
+                    if let jsonArray = rowSet.array
                     {
-                        ID = "N/A"
-                        fullName = "N/A"
-                        firstName = "N/A"
-                        lastName = "N/A"
-                        birthdate = "N/A"
-                        jerseyNumber = "N/A"
-                        position = "N/A"
-                        teamID = "N/A"
-                        let player = Players(ID: ID, fullName: fullName, firstName: firstName, lastName: lastName, birthdate: birthdate, jerseyNumber: jerseyNumber, position: position, teamID: teamID)
-                        resultArray.append(player)
-                    }
-                }
-            } else {
-                if let jsonArray = rowSet.array
-                {
-                    for item in jsonArray
-                    {
-                        teamID = item.arrayValue[0].stringValue
-                        fullName = item.arrayValue[3].stringValue
-                        fullNameArr = fullName?.components(separatedBy: " ")
-                        firstName = fullNameArr?[0]
-                        if fullNameArr?.count == 1 {
-                            lastName = fullNameArr?[0]
-                        } else {
-                            lastName = fullNameArr?[1] ?? ""
+                        for _ in jsonArray
+                        {
+                            ID = "N/A"
+                            fullName = "N/A"
+                            firstName = "N/A"
+                            lastName = "N/A"
+                            birthdate = "N/A"
+                            jerseyNumber = "N/A"
+                            position = "N/A"
+                            teamID = "N/A"
+                            let player = Players(ID: ID, fullName: fullName, firstName: firstName, lastName: lastName, birthdate: birthdate, jerseyNumber: jerseyNumber, position: position, teamID: teamID)
+                            resultArray.append(player)
                         }
-                        birthdate = item.arrayValue[8].stringValue
-                        jerseyNumber = item.arrayValue[4].stringValue
-                        position = item.arrayValue[5].stringValue
-                        ID = item.arrayValue[12].stringValue
-                        let player = Players(ID: ID, fullName: fullName, firstName: firstName, lastName: lastName, birthdate: birthdate, jerseyNumber: jerseyNumber, position: position, teamID: teamID)
-                        resultArray.append(player)
+                    }
+                } else {
+                    if let jsonArray = rowSet.array
+                    {
+                        for item in jsonArray
+                        {
+                            teamID = item.arrayValue[0].stringValue
+                            fullName = item.arrayValue[3].stringValue
+                            fullNameArr = fullName?.components(separatedBy: " ")
+                            firstName = fullNameArr?[0]
+                            if fullNameArr?.count == 1 {
+                                lastName = fullNameArr?[0]
+                            } else {
+                                lastName = fullNameArr?[1] ?? ""
+                            }
+                            birthdate = item.arrayValue[8].stringValue
+                            jerseyNumber = item.arrayValue[4].stringValue
+                            position = item.arrayValue[5].stringValue
+                            ID = item.arrayValue[12].stringValue
+                            let player = Players(ID: ID, fullName: fullName, firstName: firstName, lastName: lastName, birthdate: birthdate, jerseyNumber: jerseyNumber, position: position, teamID: teamID)
+                            resultArray.append(player)
+                        }
                     }
                 }
+                completion(resultArray, nil)
+            } catch {
+                print(error)
+                completion(nil, error)
             }
-            completion(resultArray, nil)
-        } catch {
-            print(error)
-            completion(nil, error)
-        }
+        }.resume()
     }
 }
--- Buckets: Basketball Data/Buckets: Basketball Data/API/TeamAPI.swift
+++ Buckets: Basketball Data/Buckets: Basketball Data/API/TeamAPI.swift
@@ -8,63 +8,71 @@
 
 import Foundation
 import SwiftyJSON
+import Alamofire
 
 class TeamAPI {
     func getTeamInfo(url: String, completion: @escaping (DetailTeam?, Error?) -> Void) {
         var detailTeam: DetailTeam?
-        let ID: String?
-        let city: String?
-        let name: String?
-        let conference: String?
-        let division: String?
-        let wins: String?
-        let losses: String?
-        let conferenceRank: String?
-        let divisionRank: String?
-        let yearFounded: String?
-        guard let url = URL(string: url) else { return }
-        do {
-            let data = try Data(contentsOf: url)
-            let json = try JSON(data: data)
-            _ = json["resultSets"][0]["headers"]
-            let rowSet = json["resultSets"][0]["rowSet"]
-            var teamInfo: JSON?
-            if rowSet.count == 0 {
-                ID = "N/A"
-                city = "N/A"
-                name = "N/A"
-                conference = "N/A"
-                division = "N/A"
-                wins = "N/A"
-                losses = "N/A"
-                conferenceRank = "N/A"
-                divisionRank = "N/A"
-                yearFounded = "N/A"
-                detailTeam = DetailTeam(ID: ID, city: city, name: name, conference: conference, division: division, wins: wins, losses: losses, conferenceRank: conferenceRank, divisionRank: divisionRank, yearFounded: yearFounded)
-            } else {
-                for (_, teamJson):(String, JSON) in rowSet {
-                    teamInfo = teamJson
-                    break
+        var ID: String?
+        var city: String?
+        var name: String?
+        var conference: String?
+        var division: String?
+        var wins: String?
+        var losses: String?
+        var conferenceRank: String?
+        var divisionRank: String?
+        var yearFounded: String?
+        guard let headerUrl = URL(string: url) else { return }
+        var request = URLRequest(url: headerUrl)
+        request.httpMethod = "GET"
+        request.setValue("stats.nba.com", forHTTPHeaderField:"Referer")
+        request.timeoutInterval = 60.0
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else { print(error!); return }
+            guard let data = data else { print("No data"); return }
+            do {
+                let json = try JSON(data: data)
+                _ = json["resultSets"][0]["headers"]
+                let rowSet = json["resultSets"][0]["rowSet"]
+                var teamInfo: JSON?
+                if rowSet.count == 0 {
+                    ID = "N/A"
+                    city = "N/A"
+                    name = "N/A"
+                    conference = "N/A"
+                    division = "N/A"
+                    wins = "N/A"
+                    losses = "N/A"
+                    conferenceRank = "N/A"
+                    divisionRank = "N/A"
+                    yearFounded = "N/A"
+                    detailTeam = DetailTeam(ID: ID, city: city, name: name, conference: conference, division: division, wins: wins, losses: losses, conferenceRank: conferenceRank, divisionRank: divisionRank, yearFounded: yearFounded)
+                } else {
+                    for (_, teamJson):(String, JSON) in rowSet {
+                        teamInfo = teamJson
+                        break
+                    }
+                    ID = teamInfo?.arrayValue[0].stringValue
+                    city = teamInfo?.arrayValue[2].stringValue
+                    name = teamInfo?.arrayValue[3].stringValue
+                    conference = teamInfo?.arrayValue[5].stringValue
+                    division = teamInfo?.arrayValue[6].stringValue
+                    wins = teamInfo?.arrayValue[8].stringValue
+                    losses = teamInfo?.arrayValue[9].stringValue
+                    conferenceRank = teamInfo?.arrayValue[11].stringValue
+                    divisionRank = teamInfo?.arrayValue[12].stringValue
+                    yearFounded = teamInfo?.arrayValue[13].stringValue
+                    detailTeam = DetailTeam(ID: ID, city: city, name: name, conference: conference, division: division, wins: wins, losses: losses, conferenceRank: conferenceRank, divisionRank: divisionRank, yearFounded: yearFounded)
                 }
-                ID = teamInfo?.arrayValue[0].stringValue
-                city = teamInfo?.arrayValue[2].stringValue
-                name = teamInfo?.arrayValue[3].stringValue
-                conference = teamInfo?.arrayValue[5].stringValue
-                division = teamInfo?.arrayValue[6].stringValue
-                wins = teamInfo?.arrayValue[8].stringValue
-                losses = teamInfo?.arrayValue[9].stringValue
-                conferenceRank = teamInfo?.arrayValue[11].stringValue
-                divisionRank = teamInfo?.arrayValue[12].stringValue
-                yearFounded = teamInfo?.arrayValue[13].stringValue
-                detailTeam = DetailTeam(ID: ID, city: city, name: name, conference: conference, division: division, wins: wins, losses: losses, conferenceRank: conferenceRank, divisionRank: divisionRank, yearFounded: yearFounded)
+            } catch {
+                print(error)
+                completion(nil, error)
             }
-        } catch {
-            print(error)
-            completion(nil, error)
-        }
-        if let team = detailTeam {
-            completion(team, nil)
-        }
+            if let team = detailTeam {
+                completion(team, nil)
+            }
+        }.resume()
     }
 }
 
